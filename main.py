832030b435cef4dacf5be7c99c98e72ef110b129
Surenkumar Nihalani <suren.k.n@icloud.com>
2016-03-24 23:07:21 -0700

    raccobot: Add router for commands

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ This bot is a deep learner and we mean deep.
 
 import random
 import logging
+logging.basicConfig()
 import time
 
 from telegram.ext import Updater
@@ -20,18 +21,53 @@ log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
 
-def catch_all(bot, update):
+command_name_to_fn = {}
+
+
+def on_command(command_name=None):
+    assert command_name is not None
+    def decorated_function(function):
+        command_name_to_fn[command_name] = function
+        return function
+    return decorated_function
+
+
+def catch_all_messages(bot, update):
     log.debug(repr(update))
     bot.sendMessage(update.message.chat_id, text=random.choice(VERSION_ONE_DEEP_LEARNING_MODEL))
 
 
+def router(bot, update):
+    """Since our bot runs in privacy mode off, we have to route our own messages to commands."""
+    callback = catch_all_messages
+    message_str = update.message.text
+    if message_str.startswith(bot.name):
+        # it's a command
+        command = message_str[len(bot.name):].strip().split()[0]
+        if command.startswith(u'/'):
+            command_name = command[1:]
+            callback = command_name_to_fn.get(command_name, catch_all_messages)
+        else:
+            log.info(u'unknown command: ' + command + u'. routing update to catch_all')
+
+    callback(bot, update)
+
+def register_handlers(updater):
+    updater.dispatcher.addTelegramMessageHandler(router)
+
+
+@on_command(command_name=u'hi')
+def respond_to_a_hi(bot, update):
+    bot.sendMessage(update.message.chat_id, text='hi ' + update.message.from_user.name)
+
+
 if __name__ == '__main__':
     updater = Updater(token=HTTP_TOKEN)
-    updater.dispatcher.addTelegramMessageHandler(catch_all)
-    updater.dispatcher.addTelegramRegexHandler('.*', catch_all)
+    register_handlers(updater)
+
     log.info('beginning polling...')
     try:
-        updater.start_polling()
+        updater.start_polling(clean=True)
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
