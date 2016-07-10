9ea24b521d18973b6dab3aac39314362cf5893c8
Surenkumar Nihalani <suren.k.n@icloud.com>
2016-07-09 23:30:01 -0700

    raccobot: add confirmation.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import logging
 logging.basicConfig()
 import time
 
-from telegram.ext import Updater
+from telegram.ext import Updater, MessageHandler, Filters
 
 HTTP_TOKEN = '215078551:AAEt8VcnxYhr_z1RPYXHJtWni-YgqoHZp2k'
 
@@ -21,47 +21,33 @@ log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
 
-command_name_to_fn = {}
-
-
-def on_command(command_name=None):
-    assert command_name is not None
-    def decorated_function(function):
-        command_name_to_fn[command_name] = function
-        return function
-    return decorated_function
-
-
-def catch_all_messages(bot, update):
-    log.debug(repr(update))
-    bot.sendMessage(update.message.chat_id, text=random.choice(VERSION_ONE_DEEP_LEARNING_MODEL))
-
-
 def router(bot, update):
-    """Since our bot runs in privacy mode off, we have to route our own messages to commands."""
-    callback = catch_all_messages
-    message_str = update.message.text
-    if message_str.startswith(bot.name):
-        # it's a command
-        command = message_str[len(bot.name):].strip().split()[0]
-        if command.startswith(u'/'):
-            command_name = command[1:]
-            callback = command_name_to_fn.get(command_name, catch_all_messages)
-        else:
-            log.info(u'unknown command: ' + command + u'. routing update to catch_all')
+    """Just responds to "to confirm"."""
+    message = update.message.text
+    if 'confirm' not in update.message.text or '@' not in message:
+        return
+    time.sleep(random.choice(range(10)))
+    log.debug(repr(update))
+    sentence = random.choice([
+        'confirmed.',
+        'can confirms.',
+        'confirmed by multiple sources.',
+        'the rat farm said so',
+        'looks like it.'
+    ])
+    bot.sendMessage(
+        update.message.chat_id,
+        text='@' + update.message.from_user.username + ' ' + sentence)
 
-    callback(bot, update)
 
 def register_handlers(updater):
-    updater.dispatcher.addTelegramMessageHandler(router)
+    updater.dispatcher.add_handler(
+        MessageHandler(
+            filters=[Filters.text],
+            callback=router))
 
 
-@on_command(command_name=u'hi')
-def respond_to_a_hi(bot, update):
-    bot.sendMessage(update.message.chat_id, text='hi ' + update.message.from_user.name)
-
-
-if __name__ == '__main__':
+def main():
     updater = Updater(token=HTTP_TOKEN)
     register_handlers(updater)
 
@@ -73,3 +59,9 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         log.info('quitting...')
         updater.stop()
+
+
+if __name__ == '__main__':
+    from ipdb import launch_ipdb_on_exception
+    with launch_ipdb_on_exception():
+        main()
